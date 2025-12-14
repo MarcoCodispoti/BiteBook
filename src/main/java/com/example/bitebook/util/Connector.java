83c341ee56b2adedb9f4702d1a61e710a1e1556a
MarcoCodispoti <marco01codispoti@gmail.com>
2025-12-14 23:23:22 +0100

    Pulizia finale

--- src/main/java/com/example/bitebook/util/Connector.java
+++ src/main/java/com/example/bitebook/util/Connector.java
@@ -9,6 +9,54 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Connector{
+//    private static Connector instance = null;
+//    private Connection conn = null;
+//
+//    private Connector() throws FailedDatabaseConnectionException {
+//        initializeConnection();
+//    }
+//
+//
+//    private void initializeConnection() throws FailedDatabaseConnectionException {
+//        try(InputStream input = Connector.class.getResourceAsStream("/com/example/bitebook/db.properties")){
+//            if(input == null){
+//                throw new FailedDatabaseConnectionException("db.properties files not found");
+//            }
+//            Properties properties = new Properties();
+//            properties.load(input);
+//
+//            String connectionURL = properties.getProperty("CONNECTION_URL");
+//            String username = properties.getProperty("LOGIN_USERNAME");
+//            String password = properties.getProperty("LOGIN_PASSWORD");
+//
+//            conn = DriverManager.getConnection(connectionURL,username,password);
+//        } catch(Exception e) {
+//            throw new FailedDatabaseConnectionException(e);
+//        }
+//    }
+//
+//    public static Connector getInstance() throws FailedDatabaseConnectionException{
+//        if (instance == null){
+//            instance = new Connector();
+//        }
+//        return instance;
+//    }
+//
+//
+//
+//    public Connection getConnection() throws FailedDatabaseConnectionException{
+//        try {
+//            if (conn == null || conn.isClosed()) {
+//                initializeConnection();
+//            }
+//            return conn;
+//        } catch (SQLException e) {
+//            throw new FailedDatabaseConnectionException("Unable to establish connection to database", e);
+//        }
+//    }
+
+
+
     private static Connector instance = null;
     private Connection conn = null;
 
@@ -16,10 +64,9 @@ public class Connector{
         initializeConnection();
     }
 
-
     private void initializeConnection() throws FailedDatabaseConnectionException {
-        try(InputStream input = Connector.class.getResourceAsStream("/com/example/bitebook/db.properties")){
-            if(input == null){
+        try (InputStream input = Connector.class.getResourceAsStream("/com/example/bitebook/db.properties")) {
+            if (input == null) {
                 throw new FailedDatabaseConnectionException("db.properties files not found");
             }
             Properties properties = new Properties();
@@ -29,22 +76,20 @@ public class Connector{
             String username = properties.getProperty("LOGIN_USERNAME");
             String password = properties.getProperty("LOGIN_PASSWORD");
 
-            conn = DriverManager.getConnection(connectionURL,username,password);
-        } catch(Exception e) {
+            conn = DriverManager.getConnection(connectionURL, username, password);
+        } catch (Exception e) {
             throw new FailedDatabaseConnectionException(e);
         }
     }
 
-    public static Connector getInstance() throws FailedDatabaseConnectionException{
-        if (instance == null){
+    public static synchronized Connector getInstance() throws FailedDatabaseConnectionException {
+        if (instance == null) {
             instance = new Connector();
         }
         return instance;
     }
 
-
-
-    public Connection getConnection() throws FailedDatabaseConnectionException{
+    public Connection getConnection() throws FailedDatabaseConnectionException {
         try {
             if (conn == null || conn.isClosed()) {
                 initializeConnection();
@@ -54,6 +99,7 @@ public class Connector{
             throw new FailedDatabaseConnectionException("Unable to establish connection to database", e);
         }
     }
+
 
 
 }
