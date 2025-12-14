912423c8b90e1ace721470697c25dd70004a41c1
MarcoCodispoti <marco01codispoti@gmail.com>
2025-12-14 23:25:46 +0100

    Pulizia finale

--- src/main/java/com/example/bitebook/util/Connector.java
+++ src/main/java/com/example/bitebook/util/Connector.java
@@ -9,54 +9,6 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Connector{
-//    private static Connector instance = null;
-//    private Connection conn = null;
-//
-//    private Connector() throws FailedDatabaseConnectionException {
-//        initializeConnection();
-//    }
-//
-//
-//    private void initializeConnection() throws FailedDatabaseConnectionException {
-//        try(InputStream input = Connector.class.getResourceAsStream("/com/example/bitebook/db.properties")){
-//            if(input == null){
-//                throw new FailedDatabaseConnectionException("db.properties files not found");
-//            }
-//            Properties properties = new Properties();
-//            properties.load(input);
-//
-//            String connectionURL = properties.getProperty("CONNECTION_URL");
-//            String username = properties.getProperty("LOGIN_USERNAME");
-//            String password = properties.getProperty("LOGIN_PASSWORD");
-//
-//            conn = DriverManager.getConnection(connectionURL,username,password);
-//        } catch(Exception e) {
-//            throw new FailedDatabaseConnectionException(e);
-//        }
-//    }
-//
-//    public static Connector getInstance() throws FailedDatabaseConnectionException{
-//        if (instance == null){
-//            instance = new Connector();
-//        }
-//        return instance;
-//    }
-//
-//
-//
-//    public Connection getConnection() throws FailedDatabaseConnectionException{
-//        try {
-//            if (conn == null || conn.isClosed()) {
-//                initializeConnection();
-//            }
-//            return conn;
-//        } catch (SQLException e) {
-//            throw new FailedDatabaseConnectionException("Unable to establish connection to database", e);
-//        }
-//    }
-
-
-
     private static Connector instance = null;
     private Connection conn = null;
 
